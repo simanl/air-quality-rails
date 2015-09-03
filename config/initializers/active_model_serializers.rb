5afb631f64c55a15369022020808bc4ece54d36f
Roberto Quintanilla <vov@icalialabs.com>
2015-09-03 14:56:06 -0500

    Added some comments on 'active_model_serializers.rb' initializer

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,1 +1,5 @@
+# Configure ActiveSupport to not wrap BigDecimal represetations in double-quotes:
+# ActiveSupport.encode_big_decimal_as_string = false
+
+# Configure ActiveModel::Serializer to use the json_api format:
 ActiveModel::Serializer.config.adapter = :json_api
