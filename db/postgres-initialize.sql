2880ca945a3cdc13b15ed1abdb0213e5d7367d0c
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2015-10-02 15:21:06 -0500

    Better integration and development flow with docker & docker compose using the official ruby image

--- db/postgres-initialize.sql
+++
@@ -1,8 +0,0 @@
--- This file is a reference to replicate a freshly-provisioned database like most
--- database service providers - such as Heroku Postgres, for example.
---
--- Including these instructions as rails migrations does not seem to be a good fit.
-CREATE EXTENSION hstore;
-CREATE EXTENSION dblink;
-CREATE DATABASE air_quality_development;
-CREATE DATABASE air_quality_test;
