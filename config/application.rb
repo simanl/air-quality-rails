80b3b577868121fbf334c93a9a04fc644b5e0e25
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2015-10-14 12:20:39 -0500

    Added directory 'app/demos' to the Rails autoload paths

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,8 @@ module AirQuality
 
     # We'll use Sidekiq as our ActiveJob backend:
     config.active_job.queue_adapter = :sidekiq
+
+    # Add the demos folder to the autoload paths:
+    config.autoload_paths += %W(#{config.root}/app/demos)
   end
 end
