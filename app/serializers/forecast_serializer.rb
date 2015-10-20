70dbcf3b8f165a9fa2ce898bb50e8c3c334ff832
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2015-10-19 19:25:26 -0500

    Se agregan los campos de timestamp en formato RFC822 para serializadores de Measurement y Forecast

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,5 +1,13 @@
 class ForecastSerializer < ActiveModel::Serializer
-  attributes :id, :starts_at, :ends_at
+  attributes :id, :starts_at, :starts_at_rfc822, :ends_at, :ends_at_rfc822
   attributes :ozone, :toracic_particles, :respirable_particles
   attributes :updated_at
+
+  def starts_at_rfc822
+    object.starts_at.rfc822
+  end
+
+  def ends_at_rfc822
+    object.ends_at.rfc822
+  end
 end
--- app/serializers/measurement_serializer.rb
+++ app/serializers/measurement_serializer.rb
@@ -1,9 +1,13 @@
 class MeasurementSerializer < ActiveModel::Serializer
-  attributes :id, :measured_at, :temperature, :relative_humidity,
+  attributes :id, :measured_at, :measured_at_rfc822, :temperature, :relative_humidity,
     :wind_direction, :wind_speed, :imeca_points, :precipitation
 
   attributes :carbon_monoxide, :nitric_oxide, :nitrogen_dioxide,
     :nitrogen_oxides, :ozone, :sulfur_dioxide, :suspended_particulate_matter,
     :toracic_particles, :respirable_particles
+
+  def measured_at_rfc822
+    object.measured_at.rfc822
+  end
 
 end
