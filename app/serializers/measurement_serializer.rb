72aacbd277d9136764cac75a1d6ddece02c39b68
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2015-10-28 13:55:04 -0600

    (Serializers) Added 'imeca_category' to measurement serializer

--- app/serializers/measurement_serializer.rb
+++ app/serializers/measurement_serializer.rb
@@ -1,6 +1,6 @@
 class MeasurementSerializer < ActiveModel::Serializer
   attributes :id, :measured_at, :measured_at_rfc822, :temperature, :relative_humidity,
-    :wind_direction, :wind_speed, :imeca_points, :precipitation
+    :wind_direction, :wind_speed, :imeca_points, :imeca_category, :precipitation
 
   attributes :carbon_monoxide, :nitric_oxide, :nitrogen_dioxide,
     :nitrogen_oxides, :ozone, :sulfur_dioxide, :suspended_particulate_matter,
